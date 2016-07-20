a3e2b4c0d5b7d8ece3d2d39840c45807cfb64536
Matthew Gadd <mgadd@sitepen.com>
2016-07-20 19:47:49 +0100

    support command types

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const yargs = require('yargs');
 const config = {
 	depPaths: [path.resolve(__dirname, 'node_modules'), 'node_modules'],
 	folderPrefixes: ['dojo-cli-build'],
+	commandTypes: ['build', 'template']
 }
 
 const pluginMap = new Map();
 
-function globs (depPaths, folderPrefixes) {
+function globs(depPaths, folderPrefixes) {
 	const globPaths = [];
 	depPaths.forEach((depPath) => {
 		folderPrefixes.forEach((folderPrefix) => {
@@ -22,22 +23,40 @@ function globs (depPaths, folderPrefixes) {
 globby(globs(config.depPaths, config.folderPrefixes)).then((paths) => {
 	const commands = paths.map((path) => {
 		const command = require(path);
-		const pluginName = command.name || /dojo-cli-.*-(.*)/.exec(path)[1];
+		const pluginParts = /dojo-cli-(.*)-(.*)/.exec(path);
+		const pluginType = pluginParts[1]
+		const pluginName = pluginParts[2];
 		let computedName = pluginName
 		let count = 1;
+
 		while (pluginMap.has(computedName)) {
 			computedName = `${pluginName}-${count}`;
 			count++;
 		}
+
 		pluginMap.set(computedName, command);
-		return [computedName, `${command.description} (${path})`, command.register, command.run];
+		return {
+			type: pluginType,
+			args: [
+				computedName,
+				`${command.description} (${path})`,
+				command.register,
+				command.run
+			]
+		};
 	});
-	yargs.command('build', 'production-ize your app', function(yargs) {
-		commands.map((command) => yargs.command.apply(null, command));
-		return yargs;
-	})
-	.demand(1, 'must provide a valid command')
-	.help('h')
-	.alias('h', 'help')
-	.argv
+
+	config.commandTypes.forEach((commandType) => {
+		yargs.command(commandType, '', (yargs) => {
+			commands
+				.filter((command) => command.type === commandType)
+				.map((command) => yargs.command.apply(null, command.args));
+			return yargs;
+		});
+	});
+
+	yargs.demand(1, 'must provide a valid command')
+		.help('h')
+		.alias('h', 'help')
+		.argv
 });
