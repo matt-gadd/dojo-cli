0d9325584911ba615f973d356e1c4b2c98d9e204
Matthew Gadd <mgadd@sitepen.com>
2016-07-20 21:56:14 +0100

    base name off prefix and command type

--- index.js
+++ index.js
@@ -4,32 +4,35 @@ const yargs = require('yargs');
 
 const config = {
 	depPaths: [path.resolve(__dirname, 'node_modules'), 'node_modules'],
-	folderPrefixes: ['dojo-cli-build'],
+	folderPrefixes: ['dojo-cli'],
 	commandTypes: [
+		{ name: 'new', description: 'scaffold' },
 		{ name: 'build', description: 'build all the things' },
 		{ name: 'template', description: 'get started quickly with a template' }
 	]
-}
+};
 
 const pluginMap = new Map();
 
-function globs(depPaths, folderPrefixes) {
+function globs(depPaths, folderPrefixes, commandTypes) {
 	const globPaths = [];
 	depPaths.forEach((depPath) => {
 		folderPrefixes.forEach((folderPrefix) => {
-			globPaths.push(path.resolve(depPath, folderPrefix + '-*'));
+			commandTypes.forEach((commandType) => {
+				globPaths.push(path.resolve(depPath, `${folderPrefix}-${commandType.name}-*`));
+			});
 		});
 	});
 	return globPaths;
 }
 
-globby(globs(config.depPaths, config.folderPrefixes)).then((paths) => {
+globby(globs(config.depPaths, config.folderPrefixes, config.commandTypes)).then((paths) => {
 	const commands = paths.map((path) => {
 		const command = require(path);
 		const pluginParts = /dojo-cli-(.*)-(.*)/.exec(path);
-		const pluginType = pluginParts[1]
+		const pluginType = pluginParts[1];
 		const pluginName = pluginParts[2];
-		let computedName = pluginName
+		let computedName = pluginName;
 		let count = 1;
 
 		while (pluginMap.has(computedName)) {
@@ -38,6 +41,7 @@ globby(globs(config.depPaths, config.folderPrefixes)).then((paths) => {
 		}
 
 		pluginMap.set(computedName, command);
+
 		return {
 			type: pluginType,
 			args: [
